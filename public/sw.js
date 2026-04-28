888adcaade131212c8f15cd426e8aba5e97ef879
kisungdavidpark <parkks.joyful@gmail.com>
2026-04-28 13:52:21 +0900

    앱 캐시 업데이트

--- public/sw.js
+++ public/sw.js
@@ -14,5 +14,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
+  // 개발/운영 모두 최신 배포본을 우선 사용하기 위해 캐시 저장 없이 네트워크만 사용
   event.respondWith(fetch(event.request));
 });
